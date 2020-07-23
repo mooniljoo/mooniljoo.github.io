e66cfc9fdb05cd54cb578cfa6b0d5f2a01edaf48
iljoomoon <iljoomoon@gmail.com>
2020-07-23 17:58:12 +0900

    Add Viewmode

--- components/Category.js
+++ components/Category.js
@@ -3,10 +3,7 @@ import { useState, useRef } from "react";
 
 export default function Category() {
   const [cate, setCate] = useState("");
-
-  const frontend = useRef();
-  const all = useRef();
-  const backend = useRef();
+  const [viewmode, setViewmode] = useState("rowlist");
 
   const onAll = () => {
     setCate("");
@@ -18,13 +15,19 @@ export default function Category() {
     setCate("software");
   };
 
+  const viewRowlist = () => {
+    setViewmode("rowlist");
+  };
+  const viewTile = () => {
+    setViewmode("tile");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="categoryList">
           <span
             className={`categoryList__item ${cate == "" && "selected"}`}
-            ref={all}
             onClick={onAll}
           >
             ALL
@@ -33,25 +36,51 @@ export default function Category() {
             className={`categoryList__item ${
               cate == "publishing" && "selected"
             }`}
-            ref={frontend}
             onClick={onFront}
           >
             FRONT-END
           </span>
           <span
             className={`categoryList__item ${cate == "software" && "selected"}`}
-            ref={backend}
             onClick={onBack}
           >
             BACK-END
           </span>
         </div>
-        <div className="viewList">
-          <i className="viewList__item">thum형</i>
-          <i className="viewList__item">list형</i>
+        <div className="viewmodes">
+          <i
+            className={`viewmode ${viewmode == "rowlist" && "selected"}`}
+            onClick={viewRowlist}
+          >
+            <svg
+              width="26"
+              height="26"
+              viewBox="0 0 26 26"
+              fill="#333"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M4.33331 15.1665H8.41174V10.8332H4.33331V15.1665ZM4.33331 20.5832H8.41174V16.2498H4.33331V20.5832ZM4.33331 9.74984H8.41174V5.4165H4.33331V9.74984ZM9.43135 15.1665H21.6666V10.8332H9.43135V15.1665ZM9.43135 20.5832H21.6666V16.2498H9.43135V20.5832ZM9.43135 5.4165V9.74984H21.6666V5.4165H9.43135Z" />
+            </svg>
+            <span className="screen_out">Show articles in Row List</span>
+          </i>
+          <i
+            className={`viewmode ${viewmode == "tile" && "selected"}`}
+            onClick={viewTile}
+          >
+            <svg
+              width="26"
+              height="26"
+              viewBox="0 0 26 26"
+              fill="#333"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M4.33337 13.8424H12.0371V5.4165H4.33337V13.8424ZM4.33337 20.5832H12.0371V15.5276H4.33337V20.5832ZM13.963 20.5832H21.6667V12.1572H13.963V20.5832ZM13.963 5.4165V10.4721H21.6667V5.4165H13.963Z"></path>
+            </svg>
+            <span className="screen_out">Show articles in Tile Grid</span>
+          </i>
         </div>
       </div>
-      <section className="card-container">
+      <section className={`card-container ${viewmode}`}>
         {desc.workList
           .filter((e) => e.category.includes(cate))
           .map((e, idx) => (
@@ -79,11 +108,6 @@ export default function Category() {
           ))}
       </section>
       <style jsx>{`
-        .card-container {
-          display: flex;
-          flex-direction: column;
-        }
-
         .workList__item__type {
           font-size: 1em;
           color: #1d58ff;
@@ -107,35 +131,65 @@ export default function Category() {
           justify-content: space-between;
         }
         .categoryList__item,
-        .viewList__item {
+        .viewmodes__item {
           cursor: pointer;
+          font-weight: 500;
         }
         .categoryList__item {
           margin-right: 20px;
         }
-        .viewList__item {
+        .viewmode {
           margin-left: 20px;
         }
+
         .categoryList__item:hover,
-        .categoryList__item.selected,
-        .viewList__item:hover,
-        .viewList__item.selected {
+        .categoryList__item.selected {
           border-bottom: 1px solid #1d58ff;
           color: #1d58ff;
+        }
+        .viewmode:hover > svg,
+        .viewmode.selected > svg {
+          cursor: pointer;
+          fill: #1d58ff;
         }
         .container {
           width: 1024px;
           margin: 0 auto;
         }
+
+        .card-container {
+          display: flex;
+        }
+        .card-container.rowlist {
+          flex-direction: column;
+        }
+
+        .card-container.tile {
+          flex-wrap: wrap;
+          justify-content: flex-start;
+        }
+
+        .card-container.tile .card {
+          width: 24%;
+          margin-right: 1%;
+        }
+        .card-container.tile .fig_img {
+          max-width: initial;
+          justify-content: center;
+        }
+
         .card {
           background: #fff;
           box-shadow: 0 0 0 1px #ccc;
           border-radius: 10px;
           margin-bottom: 10px;
         }
-        .card__link {
+        .card > .card__link {
           display: flex;
           padding: 10px;
+        }
+        .card-container.tile .card__link {
+          display: block;
         }
         .card__link > figure {
           flex: 1;
